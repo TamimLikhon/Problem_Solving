6aeafe3acf828353697b80fd9dde1a1e7455af6b
Tamim-Likhon <tamimlikhon456@gmail.com>
2024-05-07 01:10:53 +0600

    7/5

--- Contest/3138. Minimum Length of Anagram Concatenation.py
+++ Contest/3138. Minimum Length of Anagram Concatenation.py
@@ -1,19 +1,29 @@
+from collections import Counter
+
 class Solution:
     def minAnagramLength(self, s: str) -> int:
-        char_count = {}
-        for char in s:
-            if char in char_count:
-                char_count[char] += 1
-            else:
-                char_count[char] = 1
-        
-        for count in char_count.values():
-            if count > 1:
-                return len(s)
-        
-        return len(set(s))
+        # Get the length of the input string 's'
+        n = len(s)
 
-# Example usage
-solution = Solution()
-s = "xxe"
-print(solution.minAnagramLength(s))  # Output: 3
+        # Define a helper function to calculate the greatest common divisor (GCD)
+        def gcd(a, b):
+            # Euclidean algorithm to find GCD
+            while b != 0:
+                temp = b
+                b = a % b
+                a = temp
+            return a
+
+        # Count the occurrences of each character in 's' using Counter
+        hashmap = Counter(s)
+
+        # Initialize the divisor with the count of the first character in 's'
+        div = hashmap[s[0]]
+
+        # Iterate over the counts of characters in the hashmap
+        for key, value in hashmap.items():
+            # Update the divisor by finding the GCD of the current count and the previous divisor
+            div = gcd(div, value)
+        
+        # Return the minimum length of 't', which is the length of 's' divided by the divisor
+        return n // div
