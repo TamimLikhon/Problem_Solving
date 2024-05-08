795b7b5c67f758dc3b5a88b8b632b7985302e9fa
Tamim-Likhon <tamimlikhon456@gmail.com>
2024-05-09 00:46:36 +0600

    9/5

---
+++ 1605. Find Valid Matrix Given Row and Column Sums.py
@@ -0,0 +1,14 @@
+class Solution:
+    def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
+        rows = len(rowSum)
+        cols = len(colSum)
+
+        matrix = [[0] * cols for _ in range(rows)]
+
+        for i in range(rows):
+            for j in range(cols):
+                matrix[i][j] = min(rowSum[i], colSum[j])
+                rowSum[i] -= matrix[i][j]
+                colSum[j] -= matrix[i][j]
+
+        return matrix
--- Contest/3139. Minimum Cost to Equalize Array.py
+++ Contest/3139. Minimum Cost to Equalize Array.py
@@ -1,18 +1,10 @@
-nums = [2, 3, 3, 3, 5]
+nums = [2,3,3,3,5]
 cost1 = 2
 cost2 = 1
 
 count = 0
 tar = max(nums)
-for i in range(len(nums)):
-    while nums[i] < tar:
-        if i+1 < len(nums) and nums[i+1] < tar:
-            nums[i] += cost1
-            nums[i+1] += cost2
-            count += 1
-        else:
-            nums[i] += cost1
-            count += 1
+for i in range(len(nums) - 1):
+    nums[i] += cost1
 
-print("Number of increments:", count)
-print("Modified list:", nums)
+print(nums)
